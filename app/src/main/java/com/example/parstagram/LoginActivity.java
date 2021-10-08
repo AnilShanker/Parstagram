c3bf9314ea165062cb81089ccdfb73f6a7acaabd
Anil Shanker <the235anil@hotmail.com>
2021-10-08 17:13:34 -0500

    Added functionality to retrieve posts from DB

--- app/src/main/java/com/example/parstagram/LoginActivity.java
+++ app/src/main/java/com/example/parstagram/LoginActivity.java
@@ -64,5 +64,6 @@ public class LoginActivity extends AppCompatActivity {
     private void goMainActivity() {
         Intent i = new Intent(this, MainActivity.class);
         startActivity(i);
+        finish();
     }
 }
--- app/src/main/java/com/example/parstagram/ParseApplication.java
+++ app/src/main/java/com/example/parstagram/ParseApplication.java
@@ -3,12 +3,16 @@ package com.example.parstagram;
 import android.app.Application;
 
 import com.parse.Parse;
+import com.parse.ParseObject;
 
 public class ParseApplication extends Application {
 
     @Override
     public void onCreate() {
         super.onCreate();
+
+        ParseObject.registerSubclass(Post.class);
+
         Parse.initialize(new Parse.Configuration.Builder(this)
                 .applicationId("f3Nikgflid5CiZ18JodtBNIjE7rN2lyFKRHqlCSo")
                 .clientKey("yMuR2QVfKUh9oRmYHaNhwCesmZtUwMa0aKp8jlWl")
